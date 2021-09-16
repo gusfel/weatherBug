4e0361c5d1bebbf6e775a73064cd728f6bcfb54b
gusfel <agustinjfeliciano@gmail.com>
2021-09-16 11:45:32 -0700

    removed extra text

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -48,21 +48,6 @@ const App = () => {
         {wData.length
           ? compileData(wData)
           : 'waiting...'}
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.sdfbsdfg
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
