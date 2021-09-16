3cb476030ef8901986b6afcbe661563e588b410e
gusfel <agustinjfeliciano@gmail.com>
2021-09-15 20:35:26 -0700

    got api to work

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -1,9 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3001;
+const key = require('../keys.js')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.get('/weather', (req, res) => {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=san francisco&appid=${key}&units=imperial`;
+  axios.get(url)
+    .then(res => {
+      console.log(res.data.list.length)
+    })
+    .catch(err => {
+      console.log(err)
+    })
 })
 
 app.listen(port, () => {
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
-import express from 'express'
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   const [data, setData] = useState([]);
-  
+
+  useEffect(() => {
+    axios.get('localhost:3001/weather')
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
