3c59c15b79804c8f0aa57ddb316a82ccffeb958d
gusfel <agustinjfeliciano@gmail.com>
2021-09-15 21:41:28 -0700

    got server to call api and push info to client

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -4,12 +4,27 @@ const app = express();
 const port = 3001;
 const key = require('../keys.js')
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.get('/weather', (req, res) => {
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=san francisco&appid=${key}&units=imperial`;
   axios.get(url)
-    .then(res => {
-      console.log(res.data.list.length)
+    .then(response => {
+      const data = response.data.list
+      const final = [];
+      for (const info of data) {
+        if (info.dt_txt.slice(11, 13) === '12') {
+          const day = info.dt_txt.slice(0, 10)
+          final.push({
+            date : day,
+            info,
+          })
+        }
+      }
+      res.send(final)
     })
     .catch(err => {
       console.log(err)
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,16 +4,28 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  const [data, setData] = useState([]);
+  const [wData, setwData] = useState([]);
 
   useEffect(() => {
-    axios.get('localhost:3001/weather')
+    axios.get('http://localhost:3001/weather')
+      .then(res => {
+        setwData(res.data)
+        // for (const obj of res.data) {
+        //   setData(() => [...data, obj]
+        //   )
+          // console.log(obj)
+        // }
+      })
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {wData.length
+        // ? Object.keys(wData[0][0])
+        ? `${wData[0].info.weather[0].description}`
+        : 'hi'}
         <p>
           Edit <code>src/App.js</code> and save to reload.sdfbsdfg
         </p>
